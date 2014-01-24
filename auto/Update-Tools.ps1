454af663b7867eee46e9672772c25d28cf7dcdad
mastersign <dev@mastersign.de>
2014-01-24 14:26:10 +0100

    added script for downloading tools

---
+++ auto/Update-Tools.ps1
@@ -0,0 +1,52 @@
+$MyPath = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+
+$BasePath = Resolve-Path "$MyPath\.."
+$BasePath = "$BasePath\tools"
+if (!(Test-Path $BasePath)) {
+	mkdir $BasePath | Out-Null
+}
+
+$tmp = "$BasePath\download.tmp"
+
+$7zipUrl = "https://sourceforge.net/projects/sevenzip/files/7-Zip/9.20/7za920.zip/download"
+$7zipDir = "$BasePath\7z"
+
+$ffmpegUrl = "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z"
+$ffmpegDir = "$BasePath\FFmpeg"
+
+$wavevizUrl = "https://github.com/mastersign/waveviz/releases/download/v1.1.0/WaveViz.exe"
+$wavevizDir = "$BasePath\WaveViz"
+
+
+function unzip($filePath, $targetDir) {
+    if (!(Test-Path $targetDir)) { mkdir $targetDir | Out-Null }
+    $shellApp = New-Object -com shell.application
+    $zipFile = $shellApp.namespace($filePath)
+    $dest = $shellApp.namespace($targetDir)
+    $dest.Copyhere($zipFile.items(), 0x10)
+}
+
+function un7zip($filePath, $targetDir) {
+    & "$script:7zipDir\7za.exe" x "-o$targetDir" $filePath
+}
+
+function download($url, $targetFile) {
+    if (Test-Path $targetFile) {
+        Remove-Item $targetFile
+    }
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, $targetFile)
+}
+
+#download $7zipUrl $tmp
+#unzip $tmp $7zipDir
+#Remove-Item $tmp
+
+#download $ffmpegUrl $tmp
+#un7zip $tmp $BasePath
+#Remove-Item $tmp
+#Get-ChildItem "$BasePath\ffmpeg-*-win32-static" -Directory `
+#    | % { Move-Item $_ $ffmpegDir -Force }
+
+if (!(Test-Path $wavevizDir)) { mkdir $wavevizDir | Out-Null }
+download $wavevizUrl "$wavevizDir\WaveViz.exe"
